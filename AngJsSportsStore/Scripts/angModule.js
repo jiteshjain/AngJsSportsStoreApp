5c774248f6e27b1047828e9b0ecb948031f8b720
jiteshjain <jitesh.jain87@gmail.com>
2017-10-28 21:45:21 +0530

    10_28_2017 Show List and its category and filter by category

---
+++ AngJsSportsStore/Scripts/angModule.js
@@ -0,0 +1,21 @@
+﻿angular.module("sportsStore", ["customFilters"])
+.controller("sportsStoreCtrl", function ($scope) {
+    $scope.data = {
+        products: [{ name: "Proudct #1", description: "A Product", category: "Category #1", price: 100 },
+                    { name: "Proudct #2", description: "A Product", category: "Category #1", price: 110 },
+                    { name: "Proudct #3", description: "A Product", category: "Category #2", price: 210 },
+                    { name: "Proudct #4", description: "A Product", category: "Category #3", price: 202 }
+        ]
+    };
+});
+angular.module("sportsStore").controller("productListCtrl", function ($scope,$filter)
+{
+    var selectCategory = null;
+    $scope.selectCategory = function (newCategory) {
+        selectCategory = newCategory;
+    }
+
+    $scope.categoryFilterFn = function (product) {
+        return selectCategory == null || selectCategory == product.category;
+    }
+});
